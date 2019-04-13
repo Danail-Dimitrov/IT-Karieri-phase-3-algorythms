835a578b222bb6e38698b471dd6a8e77bf84f4a1
Danail Dimitrov <danaildimitrov321@gmail.com>
2019-04-13 12:26:31 +0300

    notes and start of task oop

---
+++ oop/ConsoleApp1/BankAccounts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class BankAccounts
+    {
+        private string iban;
+        private decimal ammount;
+
+        public void Deposit (decimal ammount)
+        {
+            this.ammount += ammount;
+        }
+
+        public void Withdraw(decimal ammount)
+        {
+            this.ammount -= ammount;
+        }
+    }
+}
---
+++ oop/ConsoleApp1/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        private int age;
+        private string name;
+        private List<BankAccounts> accaounts;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public decimal GetBalance()
+        {
+            decimal ammount;
+            return ammount;
+        }
+    }
+}
